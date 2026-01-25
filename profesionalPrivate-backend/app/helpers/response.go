73a54f24d1532f47cae5b6be275045719672f60e
restuanggia-del <241223303+restuanggia-del@users.noreply.github.com>
2026-01-25 13:40:43 +0700

    perbaikan di response.go

--- profesionalPrivate-backend/app/helpers/response.go
+++ profesionalPrivate-backend/app/helpers/response.go
@@ -9,9 +9,10 @@ type APIResponse struct {
 	Success bool        `json:"success"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+	Errors  interface{} `json:"errors,omitempty"`
 }
 
-func JSON(w http.ResponseWriter, status int, message string, data interface{}) {
+func JSON(w http.ResponseWriter, status int, message string, data interface{}, errors ...interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
@@ -19,6 +20,7 @@ func JSON(w http.ResponseWriter, status int, message string, data interface{}) {
 		Success: status >= 200 && status < 300,
 		Message: message,
 		Data:    data,
+		Errors:  errors,
 	}
 
 	json.NewEncoder(w).Encode(response)
