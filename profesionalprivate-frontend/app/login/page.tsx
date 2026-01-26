4a8282b06aa1ba39a65717bbf6f0c27820062a4e
restuanggia-del <241223303+restuanggia-del@users.noreply.github.com>
2026-01-26 22:44:15 +0700

    desain ui login

--- profesionalprivate-frontend/app/login/page.tsx
+++ profesionalprivate-frontend/app/login/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+
+import { useState } from "react";
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-lg">
+        <h1 className="text-2xl font-bold text-center mb-6 text-black">
+          Login
+        </h1>
+        <div className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium mb-1 text-black">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full border rounded-lg px-4 py-2 text-gray-900 placeholder-gray-40 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="email@example.com"
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-1 text-black">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full border rounded-lg px-4 py-2 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="••••••••"
+            />
+          </div>
+
+          <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
