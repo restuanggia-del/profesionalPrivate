8d114a1e9352820f6864731006aa1659df9c6fc4
restuanggia-del <241223303+restuanggia-del@users.noreply.github.com>
2026-02-05 08:31:01 +0700

    membuat halaman student page.tsx

---
+++ profesionalprivate-frontend/app/student/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function StudentPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-black">
+        Welcome Student 🎓
+      </h1>
+    </div>
+  )
+}
